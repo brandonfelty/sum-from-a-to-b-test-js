a67cd12b079a47057b7bbfc2005cc30d7242a81e
Brandon Felty <brcfelty@gmail.com>
2022-07-27 12:49:10 -0600

    practice with recursion

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+const { FindOperators } = require("mongodb");
 
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+  // base case
+  if (toN === fromN) {
+    return toN;
+  }
+
+  return (fromN + sum(fromN + 1, toN));
 }
 
 module.exports = sum;
